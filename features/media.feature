6b84c0862317837e26170622ba6c2f9c6fe93264
LEONARDO CARDENUTO MARI <23.01210-2@maua.br>
2024-04-22 10:28:22 -0300

    Bdd + Tdd

--- features/media.feature
+++
@@ -1,5 +0,0 @@
-Feature: Média de dois números
-    Scenario: Realizar a média simples
-        Given eu possuo dois números : 28 e 56
-        When eu somo e divido os números por 2
-        Then o resultado será 42
---
+++ tdd/src/contas.py
@@ -0,0 +1,13 @@
+class Contas:
+    
+    @staticmethod
+    def soma(a, b):
+        return a + b
+    
+    @staticmethod
+    def subtracao(a, b):
+        return a - b
+
+    @staticmethod
+    def media(a, b):
+        return (a + b)/2
