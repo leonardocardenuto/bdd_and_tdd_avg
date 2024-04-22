ff1c9f1fa9ed2a512524a4372339952596f4fff6
LEONARDO CARDENUTO MARI <23.01210-2@maua.br>
2024-04-22 10:24:43 -0300

    TDD

---
+++ bdd/steps/media.py
@@ -0,0 +1,15 @@
+from behave import given, when, then
+
+@given('eu possuo dois números : {num1:d} e {num2:d}')
+
+def step_impl(context, num1, num2):
+  context.num1 = num1
+  context.num2 = num2
+
+@when('eu somo e divido os números por 2')
+def step_impl(context):
+  context.resultado = (context.num1 + context.num2)/2
+
+@then('o resultado será {resultado:d}')
+def step_impl(context, resultado):
+  assert context.resultado == resultado, f"Resultado incorreto. Esperado: {resultado}. Obtido: {context.resultado}"
